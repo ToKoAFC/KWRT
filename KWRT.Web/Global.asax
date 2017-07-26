0f55603f38d453911a62d2e877e0cbe63e1c7b6c
Tomasz Krupa <tomaszkrupa95@gmail.com>
2017-07-26 20:27:31 +0200

    Add main project

---
+++ KWRT.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KWRT.Web.MvcApplication" Language="C#" %>
---
+++ KWRT.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KWRT.Web.Startup))]
+namespace KWRT.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
