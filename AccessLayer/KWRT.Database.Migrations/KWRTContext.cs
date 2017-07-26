beed35e06f73ed76b1f15780403e462511830b0e
Tomasz Krupa <tomaszkrupa95@gmail.com>
2017-07-26 22:42:07 +0200

    Add Database projects

---
+++ AccessLayer/KWRT.Database.Migrations/KWRTContext.cs
@@ -0,0 +1,10 @@
+﻿using KWRT.Database.Models.Models;
+using System.Data.Entity;
+
+namespace KWRT.Database.Migrations
+{
+    public class KWRTContext : DbContext
+    {
+        public DbSet<KWRTUser> Users { get; set; }
+    }
+}
---
+++ AccessLayer/KWRT.Database.Models/Models/KWRTUser.cs
@@ -0,0 +1,9 @@
+﻿namespace KWRT.Database.Models.Models
+{
+    public class KWRTUser
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
