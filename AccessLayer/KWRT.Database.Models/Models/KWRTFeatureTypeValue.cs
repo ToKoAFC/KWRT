e39b27bd588cc66c8cd9b75cdf2e4fe176d71b64
ToKoAFC <tomaszkrupa95@gmail.com>
2017-08-25 16:24:35 +0200

    add db models

---
+++ AccessLayer/KWRT.Database.Models/Models/KWRTFeatureTypeValue.cs
@@ -0,0 +1,10 @@
+﻿namespace KWRT.Database.Models
+{
+    public class KWRTFeatureTypeValue
+    {        
+        public int TypeId { get; set; }
+        public string Value { get; set; }
+        
+        public virtual KWRTFeatureType Type { get; set; }
+    }
+}
---
+++ LogicLayer/KWRT.Common/Enums/FeatureTypeEnum.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace KWRT.Common.Enums
+{
+    public enum FeatureTypeEnum
+    {
+        [DataMember]
+        Int = 1,
+        [DataMember]
+        String = 2,
+        [DataMember]
+        DateTime = 3,
+        [DataMember]
+        Bool = 4,
+        [DataMember]
+        List = 5
+    }
+}
